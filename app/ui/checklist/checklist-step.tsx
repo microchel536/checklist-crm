8518c923f74f370e48252c08be83df8c06aa6c0b
microchel536 <freeandegraund@gmail.com>
2025-05-06 23:03:11 +0300

    Update checklist-step.tsx

--- app/ui/checklist/checklist-step.tsx
+++ app/ui/checklist/checklist-step.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react"; //было import React from "react";
+import React from "react"; //было import React from "react";
 import clsx from "clsx";
 import Image from "next/image";
 import { formatCurrency } from "@/app/lib/utils";
