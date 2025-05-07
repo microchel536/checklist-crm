bd69735d9597292e33462695bc193adfda881ae4
microchel536 <freeandegraund@gmail.com>
2025-05-08 00:59:14 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
 module.exports = {
-  resolve: {
-    extensions: ['.ts', '.tsx',3. '.js'], // Поддержка расширений
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/, // Регулярное выражение для .ts и .tsx файлов
-        loader: 'ts-loader', // Используйте ts-loader для обработки TypeScript
-        options: {
-          transpileOnly: true, // Включите трансляцию без дополнительной проверки типов
-        },
-        exclude: /node_modules/, // Исключите node_modules
-  plugins: { // Изначальный текст
-    tailwindcss: {}, // .
-    autoprefixer: {}, // .
-  },
+  plugins: {
+    tailwindcss: {}, 
+    autoprefixer: {} 
+  }
 };
